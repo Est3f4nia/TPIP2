961a96a9139b2f21c54cce20a85819d32b350cf6
ValentinaBossa27 <valentinabossa274@gmail.com>
2025-06-22 21:36:32 -0300

    Create Equipo.java

---
+++ Equipo.java
@@ -0,0 +1,17 @@
+public class Equipo {
+    private String nombre;
+    private int puntos;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        this.puntos = 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+}
