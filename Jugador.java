9b52e1bcf3da2eaa2f18963a8aa88a2437ce98b6
ValentinaBossa27 <valentinabossa274@gmail.com>
2025-06-22 21:38:25 -0300

    Create Jugador.java

---
+++ Jugador.java
@@ -0,0 +1,30 @@
+public class Jugador {
+    private int dni;
+    private String nombre;
+    private String posicion;
+    private int idEquipo;
+
+    public Jugador(int dni, String nombre, String posicion, int idEquipo) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.posicion = posicion;
+        this.idEquipo = idEquipo;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+    
+    public int getidEquipo() {
+        return idEquipo;
+    }
+
+}
